0031d0121823c685a2333f0faf8b6a05321f69d1
Bjorn Donald <bjorndonaldb@yahoo.com>
2021-05-24 15:58:22 +0100

    changes

---
+++ client/src/scenes/dashboard/cards.js
@@ -0,0 +1,144 @@
+import { Fragment } from "react";
+
+const cards = {
+    crocodile: [
+        {
+            title: "The Pictograph",
+            body: `The pictograph of the
+            Hidden Crocodile is rife with
+            a variety of claw symbols
+            echoing the centrality of
+            might and power to this
+            archetype
+            The amphibious beast’s face
+            is hidden or submerged to
+            underline its adaptability to
+            multiple environments`,
+            image: "https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215717/crocodile_icon_dtr3d0.png"
+        },
+        {
+            title: "Divination",
+            body: `You are the Hidden
+            Crocodile.
+            The Hidden Crocodile is the
+            primary symbol for raw
+            power, might and majesty. `,
+            image: `https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215759/mask_va61zi.png`
+        },
+        {
+            title: "Your Powers",
+            body: <Fragment>
+                    <ul>
+                    <li>Might</li>
+                    <li>Honesty</li>
+                    <li>Idealism</li>
+                    <li>Fortune</li>
+                    <li>Hot-headedness</li>
+                    </ul></Fragment>,
+            image: `https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215667/strength_qmjnla.png`
+        },
+        {
+            title: "Defy Odds Shoes",
+            body: `We’ve designed a pair of
+            shoes to provide a physical
+            reminder of your distinct
+            personality traits within an
+            ancient African historical
+            context to support your defy
+            odds journey. `,
+            image: `https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215769/shoes_ojmsv4.png`
+        }
+    ],
+    python: [
+        {
+            title: "The Pictograph",
+            body: `The pictograph of the
+            archetype is coiled as a
+            symbol of python’s crushing
+            power which is depicted in
+            its multiple loops.
+            The ideographic
+            representation of these
+            loops also echo the cyclical
+            nature of life`,
+            image: "https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215667/snake_icon_ntqbhr.png"
+        },
+        {
+            title: "Divination",
+            body: `You are the Coiled Python.
+            The Coiled Python is the
+            primary symbol for great
+            intuition, wisdom, and
+            preparation. `,
+            image: `https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215759/mask_va61zi.png`
+        },
+        {
+            title: "Your Powers",
+            body: <Fragment><ul>
+                    <li>Intuition</li>
+                    <li>Preparation</li>
+                    <li>Greed</li>
+                    <li>Self absorbed</li>
+                    <li>Risk adverse</li>
+                </ul></Fragment>,
+            image: `https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215667/strength_qmjnla.png`
+        },
+        {
+            title: "Defy Odds Shoes",
+            body: `We’ve designed a pair of
+            shoes to provide a physical
+            reminder of your distinct
+            personality traits within an
+            ancient African historical
+            context to support your defy
+            odds journey. `,
+            image: `https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215769/shoes_ojmsv4.png`
+        }
+    ],
+    leopard: [
+        {
+            title: "The Pictograph",
+            body: `The pictograph of a leopard
+            with a smiling human face
+            represents the idea of a
+            creature which traverses the
+            human and animal form.
+            It is the knowing smile of a
+            guardian of ancient secrets,
+            grave truths and esoteric
+            knowledge`,
+            image: "https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215760/leopard_icon_ymdpuh.png"
+        },
+        {
+            title: "Divination",
+            body: `You are the Laughing
+            Leopard.
+            The Laughing Leopard is
+            the chief symbol for great
+            strength, stealth, and vision. `,
+            image: `https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215759/mask_va61zi.png`
+        },
+        {
+            title: "Your Powers",
+            body: <Fragment><ul>
+                    <li>Courage</li>
+                    <li>Vision</li>
+                    <li>Recklessness</li>
+                    <li>Rigidity</li>
+                    <li>Confrontational</li>
+                </ul></Fragment>,
+            image: `https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215667/strength_qmjnla.png`
+        },
+        {
+            title: "Defy Odds Shoes",
+            body: `We’ve designed a pair of
+            shoes to provide a physical
+            reminder of your distinct
+            personality traits within an
+            ancient African historical
+            context to support your defy
+            odds journey. `,
+            image: `https://res.cloudinary.com/thisisafrikan-com/image/upload/v1615215769/shoes_ojmsv4.png`
+        }
+    ]
+}
